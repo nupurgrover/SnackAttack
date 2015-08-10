df3280f991e838c3d2a234e44b4834a1a2af00bb
nupurgrover <nupur.grover29@gmail.com>
2015-08-10 08:38:58 +0530

    Implementing cropping functionality

--- scripts/snackPop.js
+++ scripts/snackPop.js
@@ -1,3 +1,18 @@
+var originalWidth, originalHeight;
+
+
+function init() {
+	var height = window.innerHeight;
+	document.getElementById("outerContainer").style.height = height + "px";
+};
+
+function goToChoosePicture(){
+	document.getElementById("firstPage").style.display = "none";
+	document.getElementById("secondPage").style.display = "block";
+	drawMouth();
+	init();
+};
+
 function drawMouth(){
 	var canvas = document.getElementById("mouthOutline");
     var ctx = canvas.getContext("2d");
@@ -8,88 +23,112 @@ function drawMouth(){
     path.quadraticCurveTo(200,10,290,50);
     path.quadraticCurveTo(150,210,10,50);
     ctx.lineWidth = 4;
-   
-
-    ctx.scale(3,3);
      ctx.stroke(path);
 };
 
-function goToChoosePicture(){
-	document.getElementById("firstPage").style.display = "none";
-	document.getElementById("secondPage").style.display = "block";
-	drawMouth();
-};
-
-function uploadFirst(event){
-	var showMouthClosed = document.getElementById("showMouthClosed");
-
+function upload(event, imageId, pageId, buttonId){
+	var showMouthClosed = document.getElementById(imageId);
 	var files = event.target.files;
 	var file;
+	var image = new Image();
 	var fileAsDataUrl= "";
 	if(files && files.length > 0){
 		file = files[0];
-
 		var fileReader = new FileReader();
 		fileReader.readAsDataURL(file);
         fileReader.onload = function (event) {
             showMouthClosed.src = event.target.result;
-            document.getElementById("secondPage").style.display = "none";
-			document.getElementById("thirdPage").style.display = "block";
-            fileAsDataUrl = event.target.result;
-            try {
-	         	if(fileAsDataUrl != ""){	
-		        	localStorage.setItem("mouthClosed", fileAsDataUrl);
-		        }else{
-		        	console.log("Conversion failed");
-		        }
-		    }
-		    catch (e) {
-		        console.log("Storage failed: " + e);
-		    }
-        };
-        
-	}
-};
-
-function uploadSecond(event){
-	var showMouthOpen = document.getElementById("showMouthOpen");
-
-	var files = event.target.files;
-	var file;
-	var fileAsDataUrl= "";
-	if(files && files.length > 0){
-		file = files[0];
-
-		var fileReader = new FileReader();
-		fileReader.readAsDataURL(file);
-        fileReader.onload = function (event) {
-            showMouthOpen.src = event.target.result;
-            document.getElementById("thirdPage").style.display = "none";
-			document.getElementById("fourthPage").style.display = "block";
-            fileAsDataUrl = event.target.result;
-            try {
-	         	if(fileAsDataUrl != ""){	
-		        	localStorage.setItem("mouthOpen", fileAsDataUrl);
-		        }else{
-		        	console.log("Conversion failed");
-		        }
-		    }
-		    catch (e) {
-		        console.log("Storage failed: " + e);
-		    }
-        };
-        
+            image.src = event.target.result;
+            image.onload = function(){
+            	originalWidth = this.width;
+            	originalHeight = this.height;
+            }
+            document.getElementById(pageId).style.display = "none";
+			document.getElementById(buttonId).style.display = "block";
+        };     
 	}
 };
 
 function gotToPageFive(){
 	document.getElementById("fourthPage").style.display = "none";
 	document.getElementById("fifthPage").style.display = "block";
+	init();
 }
+
+function goBackToChoosePicture(){
+	document.getElementById("buttonCollection").style.display = "none";
+	document.getElementById("secondPage").style.display = "block";
+};
+
+function cropAndSave(){
+	document.getElementById("thirdPage").style.display = "block";
+	saveCroppedPicture("showMouthClosed", "mouthClosedOverlay", "mouthClosedContainer", "mouthClosed");
+	document.getElementById("secondPage").style.display = "none";
+	document.getElementById("buttonCollection").style.display = "none";
+};
+
+function cropAndSaveSecond(){
+	document.getElementById("fourthPage").style.display = "block";
+	saveCroppedPicture("showMouthOpen", "mouthOpenOverlay", "mouthOpenContainer", "mouthOpen");
+	document.getElementById("thirdPage").style.display = "none";
+	document.getElementById("buttonCollectionOne").style.display = "none";
+};
+
+function saveCroppedPicture(imageId, overlayId, containerId, storageId){
+	
+	var imgTarget = document.getElementById(imageId);
+	var img = $(imgTarget);
+
+	var left=$("#"+overlayId).offset().left - $("#"+containerId).offset().left;
+	var top=$("#"+overlayId).offset().top - $("#"+containerId).offset().top;
+	var width = $("#"+overlayId).width();
+	var height = $("#"+overlayId).height();
+
+	var relativeLeft = left * (originalWidth / img.width());
+	var relativeTop = top *(originalHeight / img.height());
+	var relativeW = (originalWidth / img.width()) * width;
+	var relativeHt = (originalHeight / img.height()) * height;
+
+	var tempCanvas= document.createElement("canvas"); 
+    tempCanvas.width = originalWidth ;
+    tempCanvas.height = originalHeight; 
+    var tempCtx = tempCanvas.getContext('2d'); 
+    tempCtx.drawImage(imgTarget, 0, 0); 
+
+    var imageData = tempCtx.getImageData(relativeLeft,relativeTop, relativeW, relativeHt);
+
+    var newCanvas= document.createElement("canvas"); 
+    newCanvas.width = relativeW ;
+    newCanvas.height = relativeHt; 
+    newCanvas.getContext("2d").putImageData(imageData, 0, 0);
+
+	var cropCanvas = document.getElementById("canvas");
+	cropCanvas.width = width;
+	cropCanvas.height = height;
+
+	var ctx = cropCanvas.getContext("2d");
+	ctx.scale(0.1, 0.1);                    
+    ctx.drawImage(newCanvas, 0, 0);
+
+
+	var fileAsDataUrl = cropCanvas.toDataURL("image/jpg");
+           
+    try {
+     	if(fileAsDataUrl != ""){	
+        	localStorage.setItem(storageId, fileAsDataUrl);
+        }else{
+        	console.log("Conversion failed");
+        }
+    }
+    catch (e) {
+        console.log("Storage failed: " + e);
+    }
+};
 
 function startGame(){
 	window.location.href = "gamePage.html"
 };
+
 
 function startTimer(){
 	loadCatcher();
@@ -156,3 +195,4 @@ function loadCatcher(){
 		}
 	});
 };
+
